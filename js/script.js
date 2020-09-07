12e580dbe6502b3b190f0972676a9cf52ecf69df
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-09-07 01:08:26 -0300

    [NEW] custom control

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
 
-slide.addControl();
+slide.addControl('.custom-controls');
 
 console.log(slide);
