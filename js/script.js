aa18f7faeb372d6fd28ed50fc04acf1758b4e95a
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-09-07 01:14:12 -0300

    [CHG] botões estilizados

--- js/script.js
+++ js/script.js
@@ -3,7 +3,4 @@ import { SlideNav } from './slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-
 slide.addControl('.custom-controls');
-
-console.log(slide);
